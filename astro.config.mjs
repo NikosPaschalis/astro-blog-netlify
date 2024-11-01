ccf56f0430e5fdfb2cfbd9aed1db456f57d229dd
Nikos Paschalis <nikospasxalis94@gmail.com>
2024-11-01 09:56:22 +0200

    change from md to mdx files for youtube video support fix layout to support the view of video install auto-import and astro-embed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
@@ -6,10 +7,30 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
+import AutoImport from "astro-auto-import";
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [
+    AutoImport({
+      imports: [
+        /**
+         * Generates:
+         * import Intro from './src/components/Intro.astro';
+         */
+        "./src/components/Header.astro",
+        "./src/layouts/Layout.astro",
+        "./src/layouts/PostDetails.astro",
+        /**
+         * Generates:
+         * import { YouTube } from 'astro-embed';
+         */
+        { "astro-embed": ["YouTube"] },
+      ],
+    }),
+    mdx(),
     tailwind({
       config: {
         applyBaseStyles: false,
